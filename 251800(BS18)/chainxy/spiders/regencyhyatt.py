6926d1c4648bc5a405e2a66c727ac73343e7c3b1
coseasonruby <coseasonruby@gmail.com>
2017-07-24 08:20:07 -0400

    all spiders

--- 251800(BS18)/chainxy/spiders/regencyhyatt.py
+++ 251800(BS18)/chainxy/spiders/regencyhyatt.py
@@ -10,7 +10,6 @@ from lxml import etree
 from selenium import webdriver
 from lxml import html
 import usaddress
-import pdb
 
 class regencyhyatt(scrapy.Spider):
 	name = 'regencyhyatt'
@@ -27,88 +26,69 @@ class regencyhyatt(scrapy.Spider):
 		yield scrapy.Request(url=init_url, headers=header, method='get', callback=self.body) 
 
 	def body(self, response):
-		print("=========  Checking.......")
-		with open('response.html', 'wb') as f:
-			f.write(response.body)
-
-		store_list = json.loads(response.body)['HOTELS']
-		for con_name, continent in store_list:
-			for cou_name, country in continent:
-				for sto_name, store in country['NotAvailable']:
-					item = ChainItem()
-					item['store_name'] = self.validate(store['HOTEL_NAME'])
-					# item['address'] = self.validate(store['address'])
-					# item['address2'] = self.validate(store['address2'])
-					item['city'] = self.validate(store['SPIRIT_CODE'])
-					item['state'] = self.validate(store['HOTEL_STATE'])
-					# item['zip_code'] = self.validate(store['zip'])
-					item['country'] = self.validate(store['HOTEL_COUNTRY'])
-					item['latitude'] = self.validate(store['LATITUDE'])
-					item['longitude'] = self.validate(store['LONGITUDE'])
-					link = self.validate(store['HOTEL_URL'])
-					yield scrapy.Request(url=link, callback=self.parse_page)
-				except:
-					pdb.set_trace()		
+		try:
+			store_list = json.loads(response.body)['HOTELS']
+			for con_name, continent in store_list.items():
+				for cou_name, country in continent.items():
+					if 'NotAvailable' in country:
+						for sto_name, store in country['NotAvailable'].items():
+							try:
+								item = ChainItem()
+								item['store_name'] = self.validate(store['HOTEL_NAME'])
+								item['country'] = self.validate(store['HOTEL_COUNTRY'])
+								item['latitude'] = self.validate(store['LATITUDE'])
+								item['longitude'] = self.validate(store['LONGITUDE'])
+								link = self.validate(store['HOTEL_URL'])
+								yield scrapy.Request(url=link,  callback=self.parse_page, meta={'item':item})
+							except:
+								pass
+					else:
+						for sta_name, state in country.items():
+							for sto_name, store in state.items():
+								try:
+									item = ChainItem()
+									item['store_name'] = self.validate(store['HOTEL_NAME'])
+									item['country'] = self.validate(store['HOTEL_COUNTRY'])
+									item['latitude'] = self.validate(store['LATITUDE'])
+									item['longitude'] = self.validate(store['LONGITUDE'])
+									link = self.validate(store['HOTEL_URL'])
+									yield scrapy.Request(url=link,  callback=self.parse_page, meta={'item':item})
+								except:
+									pass
+		except:
+			pass					
 
 	def parse_page(self, response):
-		
+		try:
+			item = response.meta['item']
+			detail = self.eliminate_space(response.xpath('//div[@class="addresspanel"]//p[@class="address"]//text()').extract())
+			item['address'] = detail[0]
+			item['city'] = detail[1].split(',')[0]
+			try:
+				item['state'] = detail[1].split(',')[1]
+			except:
+				pass
+			try:
+				item['zip_code'] = detail[3]
+			except:
+				pass
+			item['phone_number'] = self.validate(response.xpath('//div[@class="addresspanel"]//p[@class="phnNo"]/text()').extract_first())
+			if ':' in item['phone_number']:
+				item['phone_number'] = item['phone_number'].split(':')[1].strip()
+			yield item
+		except:
+			pass
+
 
 	def validate(self, item):
 		try:
-			return item.strip()
+			return item.strip().replace('\xa0','')
 		except:
 			return ''
 
 	def eliminate_space(self, items):
 		tmp = []
 		for item in items:
-			if self.validate(item) != '':
+			if self.validate(item) != '' and self.validate(item) != ',':
 				tmp.append(self.validate(item))
 		return tmp
-
-	def str_concat(self, items, unit):
-		tmp = ''
-		for item in items[:-1]:
-			if self.validate(item) != '':
-				tmp += self.validate(item) + unit
-		tmp += self.validate(items[-1])
-		return tmp
-
-	def check_country(self, item):
-		for state in self.US_CA_States_list:
-			if item.lower() in state['abbreviation'].lower():
-				return state['country']
-		return ''
-
-	def get_state(self, item):
-		for state in self.US_States_list:
-			if item.lower() in state['name'].lower():
-				return state['abbreviation']
-		return ''
-
-	def format(self, item):
-		try:
-			return unicodedata.normalize('NFKD', item).encode('ascii','ignore').strip()
-		except:
-			return ''
-
-	def fixLazyJson (self, in_text):
-		tokengen = tokenize.generate_tokens(StringIO(in_text).readline)
-		result = []
-		for tokid, tokval, _, _, _ in tokengen:
-			if (tokid == token.NAME):
-				if tokval not in ['true', 'false', 'null', '-Infinity', 'Infinity', 'NaN']:
-					tokid = token.STRING
-					tokval = u'"%s"' % tokval
-			elif (tokid == token.STRING):
-				if tokval.startswith ("'"):
-					tokval = u'"%s"' % tokval[1:-1].replace ('"', '\\"')
-			elif (tokid == token.OP) and ((tokval == '}') or (tokval == ']')):
-				if (len(result) > 0) and (result[-1][1] == ','):
-					result.pop()			
-			elif (tokid == token.STRING):
-				if tokval.startswith ("'"):
-					tokval = u'"%s"' % tokval[1:-1].replace ('"', '\\"')
-			result.append((tokid, tokval))
-
-		return tokenize.untokenize(result)
